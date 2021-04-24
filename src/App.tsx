ec6e803584f9f5147ab444d3bbaaf9fec3738be8
tomobre <tomobre@gmail.com>
2021-04-24 00:56:23 -0300

    added transition group for routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { Route } from "react-router-dom";
+import { Route, useLocation, Switch } from "react-router-dom";
 import Navbar from "./components/commons/Navbar";
 import Footer from "./components/commons/Footer";
 import Services from "./components/Services";
@@ -8,18 +8,31 @@ import Team from "./components/Team";
 import Jobs from "./components/Jobs";
 import Contact from "./components/Contact";
 import Home from "./components/Home";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 
 function App() {
+  const location = useLocation();
+  let routes = (
+    <TransitionGroup>
+      <CSSTransition timeout={500} classNames="route" key={location.key}>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/services" component={Services} />
+          <Route exact path="/team" component={Team} />
+          <Route exact path="/jobs" component={Jobs} />
+          <Route exact path="/contact" component={Contact} />
+        </Switch>
+      </CSSTransition>
+    </TransitionGroup>
+  );
   return (
-    <div>
-      <Route path="/" component={Navbar} />
-      <Route exact path="/" component={Home} />
-      <Route exact path="/services" component={Services} />
-      <Route exact path="/team" component={Team} />
-      <Route exact path="/jobs" component={Jobs} />
-      <Route exact path="/contact" component={Contact} />
-      <Route path="/" component={Footer} />
-    </div>
+    <>
+      <Navbar></Navbar>
+      <Switch>
+        <div>{routes}</div>;
+      </Switch>
+      <Footer></Footer>
+    </>
   );
 }
 
